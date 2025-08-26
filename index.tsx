a7c60c9b31e0f809c9475f41969f73db6000fae1
laurelis24 <lauris-kairo@inbox.lv>
2025-08-27 00:38:55 +0300

    dadded more images to test

--- index.tsx
+++ index.tsx
@@ -16,6 +16,26 @@ const images = [
     id: 4,
     src: 'https://w0.peakpx.com/wallpaper/203/1010/HD-wallpaper-forest-flora-forest-vithurshan-dark-europe-faded-green-leaf-mood-moody-sri-lanka-vithurshan-jpeg-wood.jpg',
   },
+  {
+    id: 5,
+    src: "https://images.unsplash.com/photo-1755877379664-2f809909cbec?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    id: 6,
+    src: "https://images.unsplash.com/photo-1755398104003-67d116e62f38?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    id: 7,
+    src: "https://images.unsplash.com/photo-1741557571786-e922da981949?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    id: 8,
+   src: "https://images.unsplash.com/photo-1754901350480-c0fdd1a427b4?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    id: 9,
+   src: "https://images.unsplash.com/photo-1750688650392-42b653fb50d6?q=80&w=1106&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
 ];
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
