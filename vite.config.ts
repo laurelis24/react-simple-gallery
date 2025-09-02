6f1e006a42799914e09fe4c5f8ad5c327b67f495
laurelis24 <lauris-kairo@inbox.lv>
2025-09-03 02:52:02 +0300

    deploy github

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: 'https://laurelis24.github.io/react-simple-gallery/',
+  build: {
+    outDir: "docs"
+  }
 });
